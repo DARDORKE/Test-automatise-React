499bfc7c72b65ae4ee4041c37fce55acc97ddf49
Kévy DARDOR <kevy.dardor@gmail.com>
2022-08-09 13:19:38 +0200

    React testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,13 @@
-describe('Suite de tests', () => {
-  test('Vrai est vrai', () => {
-    expect(true).toBe(true);
+import React from "react";
+import { render, screen } from '@testing-library/react';
+
+import TestScreen from "./screens/TestScreen";
+
+describe('Test des composants', () => {
+  test('TestScreen', () => {
+    render(<TestScreen/>);
+    
+    expect(screen.getByText(/Hel/)).toBeInTheDocument();
+    expect(screen.getByText('Hello')).toBeInTheDocument();
   });
-
-  test('Faux est faux', () => {
-    expect(false).toBe(false);
-  });
-});
-
-const sum = (x, y) => {
-  return x + y;
-};
-
-test('Test individuel somme', () => {
-  expect(sum(5, 8)).toBe(13);
 });
---
+++ src/screens/TestScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const title = 'Hello';
+
+const TestScreen = () => {
+  return (
+    <div>{title}</div>
+  );
+};
+
+export default TestScreen;
